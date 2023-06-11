c1156f7f479a318f05070553d7ad327079539dc4
daolanfler <daolanfler@gmail.com>
2023-06-11 18:30:21 +0800

    chore: add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
-
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable Solid to support Solid JSX components.
-  integrations: [solid(), mdx(), tailwind()]
+  integrations: [solid(), mdx(), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
