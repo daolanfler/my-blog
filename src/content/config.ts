a9309798cfeec0f43a7f6a693d3ff85b7f924aab
daolanfler <daolanfler@gmail.com>
2023-06-12 22:10:59 +0800

    feat: add disqus

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection, reference, z } from 'astro:content';
+import { defineCollection, reference, z } from "astro:content";
 // 2. Define your collection(s)
 
 const blogCollection = defineCollection({
@@ -8,15 +8,15 @@ const blogCollection = defineCollection({
     title: z.string(),
     pageTitle: z.string().optional(),
     tags: z.array(z.string()).optional(),
-    date: z.date(),
-    relatedPosts: z.array(reference('blog')).optional()
-  })
+    date: z.date().transform((v) => new Date(v.valueOf() - 8 * 60 * 60 * 1000)), // 东八区 to UTC
+    updateDate: z.date().transform((v) => new Date(v.valueOf() - 8 * 60 * 60 * 1000)).optional(), // 东八区 to UTC
+    relatedPosts: z.array(reference("blog")).optional(),
+  }),
 });
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 
-
 export const collections = {
-  'blog': blogCollection,
+  blog: blogCollection,
 };
