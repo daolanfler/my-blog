a4a05073cda95e427a278feda94a4d039b70b260
daolanfler <daolanfler@gmail.com>
2023-06-28 20:14:28 +0800

    chore: generate ui index from folder

---
+++ src/pages/ui/list.json.ts
@@ -0,0 +1,13 @@
+import { APIRoute } from "astro";
+
+// Outputs: /builtwith.json
+export const get: APIRoute = async  ({params, request}) => {
+    
+
+    return {
+      body: JSON.stringify({
+        name: 'Astro',
+        url: 'https://astro.build/',
+      }),
+    };
+  }
