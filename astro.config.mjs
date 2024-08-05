1e8c836bc18b3121a2c4369dfb747e2d6dd680d9
daolanfler <daolanfler@gmail.com>
2024-08-05 12:41:33 +0800

    chore: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,21 @@ import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 import vercel from "@astrojs/vercel/serverless";
 import { autolinkConfig } from "./plugins/rehype-autolink-config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   // Enable Solid to support Solid JSX components.
-  integrations: [solid(), mdx(), tailwind()],
+  integrations: [solid(), mdx(), tailwind(), sitemap()],
   output: "server",
   adapter: vercel(),
   markdown: {
-    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, autolinkConfig]],
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, autolinkConfig]]
   },
-  site: "https://daolanfler.xyz",
+  site: "https://daolanfler.xyz"
 });
