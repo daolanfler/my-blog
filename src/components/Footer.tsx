45bdf33d1661e98472c630ae13bdc89967a84cf7
daolanfler <daolanfler@gmail.com>
2024-08-09 11:36:10 +0800

    add new post

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <footer class="py-5 h-16 pt-8">
+    <footer class="py-10 h-16">
       <div class="copyright flex items-center justify-center">
-        <span class="dark:text-gray-300">&copy; 2022~2023</span>
+        <span class="dark:text-gray-300">&copy; 2022~2024</span>
         &nbsp;
         {/* <Icon icon="el:heart" class="text-red-600" /> */}
         &nbsp;
