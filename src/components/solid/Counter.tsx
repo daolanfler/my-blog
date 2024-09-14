18f3a6447798a1cce9099f92db53046be780f184
daolanfler <daolanfler@gmail.com>
2024-09-14 17:35:21 +0800

    update

--- src/components/solid/Counter.tsx
+++ src/components/solid/Counter.tsx
@@ -1,18 +1,23 @@
 /** @jsxImportSource solid-js */
 
+// this is just an example component 
+// to make this component work in mdx, use client:load directive 
+// for more info: https://docs.astro.build/en/concepts/islands/
 import { createSignal } from "solid-js";
 import "./Counter.css";
 
-export default function Counter(props: any) {
+export function Counter(props: any) {
   const [count, setCount] = createSignal(0);
-  const add = () => setCount(count() + 1);
+  const add = () => {
+    setCount(count() + 1)
+  };
   const subtract = () => setCount(count() - 1);
 
   return (
     <>
       <div class="counter">
         <button onClick={subtract}>-</button>
-        <pre>{count()}</pre>
+        <span>{count()}</span>
         <button onClick={add}>+</button>
       </div>
       <div class="counter-message">{props.children}</div>
