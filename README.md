80fb1977734ec3ac70d5bf11b35024785e52c53e
daolanfler <daolanfler@gmail.com>
2025-03-11 10:40:08 +0800

    chore: update to astro 5

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# daolanfler.blog 
-
-## TODO list
-
-- [ ] ui index 页面使用 [static endpoints](https://docs.astro.build/en/core-concepts/endpoints/#static-file-endpoints)
-      获取页面子路由
-
-- [ ] 博客首页的分页
+<https://daolanfler.blog> 
--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import { autolinkConfig } from "./plugins/rehype-autolink-config";
 import sitemap from "@astrojs/sitemap";
 
@@ -24,7 +24,7 @@ export default defineConfig({
     sitemap(),
     react(),
   ],
-  output: "hybrid",
+  output: "static",
   adapter: vercel(),
   markdown: {
     rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, autolinkConfig]],
